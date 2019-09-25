0f793bce15bf9cf8d8a8cb364fc64227120c1dfa
tedesoft <54484854+tedesoftco@users.noreply.github.com>
2019-09-24 21:09:08 -0500

    perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './profile/info/info.component';
+import { IntroComponent } from './profile/intro/intro.component';
+import { KnowledgeComponent } from './profile/knowledge/knowledge.component';
+import { SkillsComponent } from './profile/skills/skills.component';
+import { ExperienceComponent } from './profile/experience/experience.component';
+import { EducationComponent } from './profile/education/education.component';
+import { ContactComponent } from './profile/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    IntroComponent,
+    KnowledgeComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
